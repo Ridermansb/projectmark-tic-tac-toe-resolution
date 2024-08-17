601023bbcba85762595aabbb6f306939d09d9cd8
Riderman de Sousa Barbosa <725337+Ridermansb@users.noreply.github.com>
2024-08-17 20:20:50 -0300

    implement start new match button

--- src/PlayAgain.test.js
+++ src/PlayAgain.test.js
@@ -1,8 +1,27 @@
 import { mount } from "@vue/test-utils";
 import PlayAgain from "./PlayAgain.vue";
+import { factory } from "./test-utils";
+import { store } from "./useStore";
+
+beforeEach(() => {
+  store.startANewMatch();
+});
 
 test("<PlayAgain> should display the text correctly", () => {
   const wrapper = mount(PlayAgain);
 
   expect(wrapper.text()).toContain("Play Again");
+});
+
+test("<PlayAgain> once click should start a new match", async () => {
+  const wrapper = factory(PlayAgain, {
+    store: {
+      board: ["x", "o", "x", "o", "x", "", "x", "", "x", ""],
+      currentPlayer: "",
+    },
+  });
+
+  await wrapper.trigger("click");
+
+  expect(store.board).toStrictEqual(["", "", "", "", "", "", "", "", ""]);
 });
--- src/PlayAgain.vue
+++ src/PlayAgain.vue
@@ -1,5 +1,9 @@
+<script setup>
+import useStore from './useStore'
+const store = useStore();
+</script>
 <template>
-  <button class="play-again">
+  <button class="play-again" @click="store.startANewMatch()">
     Play Again
   </button>
 </template>
